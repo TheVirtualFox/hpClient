8d7b927efe349d8f268d14a33c351aa7becf8366
Yevgeniy Verchenko <verchenkoyevgeniy@gmail.com>
2025-06-05 00:33:20 +0300

    init

--- src/pages/Preset/index.jsx
+++ src/pages/Preset/index.jsx
@@ -1,14 +1,8 @@
 import 'swiper/css';
 import {PresetForm} from "./PresetForm/index.jsx";
 
-
 // value: 777
-
-
 export const PresetPage = () => {
-    console.log("PresetPage");
-
-
     return <div className="p-2">
         <PresetForm />
     </div>;
