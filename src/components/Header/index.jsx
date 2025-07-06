291e3922d1bc3e299902d4caa5b6b5b13192cf06
Yevgeniy Verchenko <verchenkoyevgeniy@gmail.com>
2025-07-06 13:30:28 +0300

    init

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,11 +8,12 @@ import {Button} from "flowbite-react";
 
 export const Header = () => {
     const {back, label} = useRoute();
+    const width = label.length * 12 + 24 + 24 + (!!back ? (24 + 12) : 0) + 22;
     return (<div className="h-full bg-gray-700 flex justify-center items-center">
             <div className="max-w-lg px-3 w-full flex justify-between items-center">
                 <div
-                    style={{width: `${label.length * 16 + 31 + (!!back ? 30 : 0) }px`} }
-                    className={`transition-all duration-300 ease-in-out animate-pulse1 w-52 bg-green-400 h-[50px] absolute top-0 -left-[50px] -skew-x-[25deg]`}
+                    style={{width: `${width}px`} }
+                    className={`transition-all duration-500 ease-in-out animate-pulse1 w-52 bg-green-400 h-[50px] absolute top-0 -left-[50px] -skew-x-[25deg]`}
                 ></div>
 
                 <div className="flex gap-2 items-center z-1 relative items-center">
